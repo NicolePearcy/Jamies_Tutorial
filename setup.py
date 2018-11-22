4af18ec1f81b188b3d7032a3c4d37bd6e0644697
NicolePearcy <nicole.pearcy@nottingham.ac.uk>
2018-11-22 12:35:09 +0000

    added setup file

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name = "tutorial",
+    version = "0.0.1",
+    description = "learning how to git hub",
+    zip_safe = False,
+    author = "Nicole Pearcy",
+    author_email = "nicole.pearcy@nottingham.ac.uk",
+    url = "",
+    license = "Apache",
+    packages = find_packages(),
+
+)
